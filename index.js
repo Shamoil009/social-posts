9c74fd24e252d8c2aaa3b3587f7b0d6aee6d20ea
Shamoil009 <77117702+Shamoil009@users.noreply.github.com>
2024-09-04 22:29:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const axios = require('axios');
 
-const USER_ID = '61553251963819'; // Replace with your Facebook User ID
-
-// const USER_ID = '1386080598934973'; // Replace with your Facebook User ID
-
+const USER_ID = 'process.env.FB_USER_ID'; // Replace with your Facebook User ID
 
 // const ACCESS_TOKEN = '1386080598934973|H8PRo_z2XisYJ8qh77RwXG3vpUg'; // Replace with your access token
-const ACCESS_TOKEN = 'EAAM2GMsXmGwBOwZB7pGuGIwpQzTKTeF0HyDjmDKxtqjPhxr3wTji1AZCHts7K9PuLfhj3NTZBytluTAgCAO1Mrqb3dQKpWsM1PSqpAFKFsRA4FnK2i8H2FhthoBgCykZC1uuPrp1M0dtEmAfxd6IigLA4z0PZCaDvaQrKzLIUVBg8OuhPMCkFn2o84wqOYE4tYho5bDUywdJdfvqTEXf4ZA3BPt9jMWDL450ED'; // Replace with your access token
+const ACCESS_TOKEN = process.env.FB_ACCESS_TOKEN; // Replace with your access token
 
 
 const postData = {
